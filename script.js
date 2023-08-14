f922132de181557a27bf1406d66149345d42e5d1
JosiahPThompson <josiahpaulthompson@gmail.com>
2023-08-14 12:21:19 -0500

    added code. nothing is updating on live server

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 // Write your JavaScript code here!
 
+const { pickPlanet, addDestinationInfo, formSubmission, myFetch } = require("./scriptHelper");
+
 window.addEventListener("load", function() {
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+       let selectedPlanet = pickPlanet(listedPlanets);
+   addDestinationInfo(
+    document,
+    selectedPlanet.name,
+    selectedPlanet.diameter,
+    selectedPlanet.star,
+    selectedPlanet.distance,
+    selectedPlanet.moons,
+    selectedPlanet.imageUrl
+   );
+   });
    
+   const form = document.querySelector("form");
+   form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const pilotName = document.querySelector("input[name='pilotName']").value;
+    const copilotName = document.querySelector("input[name='copilotName']").value;
+    const fuelLevel = document.querySelector("input[name='fuelLevel']").value;
+    const cargoMass = document.querySelector("input[name='cargoMass']").value;
+
+    formSubmission(document, pilotName, copilotName, fuelLevel, cargoMass);
+   });
 });
